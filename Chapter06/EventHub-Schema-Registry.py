37aaa14239b9457b3065f3c389741b8b3e70c444
Surendra <47914424+SurendraMettapalli@users.noreply.github.com>
2024-04-11 15:06:35 +0100

    Add files via upload

---
+++ Chapter06/EventHub-Schema-Registry.py
@@ -0,0 +1,39 @@
+# Registering A Schema With Schema Registry
+
+#Import the necessary libraries
+from azure.schemaregistry import SchemaRegistryClient
+from azure.identity import DefaultAzureCredential
+
+# Define Your Schema
+sampleSchema = 
+"""{
+    "namespace": "com.azure.sampleschema.avro",
+    "type": "record",
+    "name": "Trip",
+    "fields":  [
+                {"name": "tripId", "type": "string"},
+                {"name": "startLocation", "type": "string"},
+                {"name": "endLocation", "type": "string  "}
+               ]
+}"""
+
+# Create the Schema Registry Client
+azureCredential = DefaultAzureCredential()
+schema_registry_client = SchemaRegistryClient
+    (
+        fully_qualified_namespace=<SCHEMA-NAMESPACE>.servicebus.windows.net,
+        credential=azureCredential
+    )
+
+#Register the Schema
+with schema_registry_client:
+    schema_properties = schema_registry_client.register_schema
+    (
+    <SCHEMA_GROUPNAME>,
+    <SCHEMA_NAME>,
+    sampleSchema,
+    "Avro  "
+    )
+
+#Get The Schema ID
+schema_id = schema_properties.id
