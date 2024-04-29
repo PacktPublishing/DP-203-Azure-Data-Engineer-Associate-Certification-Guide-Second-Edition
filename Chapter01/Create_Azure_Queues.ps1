89a16b68ea68e812ca6e7481397a4b393848a6a3
Surendra <47914424+SurendraMettapalli@users.noreply.github.com>
2024-04-29 23:13:33 +0100

    Add files via upload

--- Chapter01/Create_Azure_Queues.ps1
+++ Chapter01/Create_Azure_Queues.ps1
@@ -1,17 +1,16 @@
-#Creating Azure File shares
+#Creating Azure Queues using the CLI
 
 $resourceGroup = "<INSERT RESOURCE GROUP NAME>"
 $storageAccount ="<INSERT STORAGE ACCOUNT NAME>"
 $storageKey = "<INSERT STORAGE KEY>"
 # Note: It is not recommended to store passwords or access keys in code files.
-# Please use Microsoft Entra ID and Azure Key Vault to store secrets
+# Please use AAD accounts and Azure Key Vault to store secrets
 $region = "<INSERT REGION NAME>"
-$fileshareName = "<INSERT FILE SHARE NAME>"
-
+$queueName = "<INSERT QUEUE NAME>"
 
 #    The following variations are accepted for setting the Key:
 #    (1) account name and key (--account-name and --account-key options or
-#        set AZURE_STORAGE_ACCOUNT and AZURE_STORAGE_KEY environment variables)
+#       set AZURE_STORAGE_ACCOUNT and AZURE_STORAGE_KEY environment variables)
 #    (2) account name and SAS token (--sas-token option used with either the --account-name
 #        option or AZURE_STORAGE_ACCOUNT environment variable)
 #    (3) account name (--account-name option or AZURE_STORAGE_ACCOUNT environment variable;
@@ -24,13 +23,11 @@ $fileshareName = "<INSERT FILE SHARE NAME>"
 $env:AZURE_STORAGE_ACCOUNT=$storageAccount
 $env:AZURE_STORAGE_KEY=$storageKey
 
-#You can create a new Azure File Share for IAC using the share-rm create option:
-az storage share-rm create --resource-group $resourceGroup --storage-account $storageAccount --name $fileshareName --quota 1024
-#You can list the file shares using the share list option:
-az storage share list --account-name $storageAccount
-#You can put a file into our File share using the file upload option:
-az storage file upload --share-name $fileshareName --source Data/testfile.txt
-#You can view the files in your File share using file list:
-az storage file list --share-name $fileshareName
-#Finally, you can download the file that we previously uploaded using the file download option:
-az storage file download --share-name $fileshareName -p testfile.txt --dest ./testfile.txt
+#You can create a new Azure queue using the storage queue create command:
+az storage queue create --name $queueName --account-name $storageAccount
+#You can easily list the queues under a storage account using the storage queue list term:
+az storage queue list --account-name $storageAccount
+#You can add a new message to the newly created Queue using the storage message put option:
+az storage message put --queue-name $queueName --content "test"
+#Finally, use the storage message peek command to view the message. This command retrieves one or more messages from the front of the queue but does not alter the visibility of the message:
+az storage message peek --queue-name $queueName
