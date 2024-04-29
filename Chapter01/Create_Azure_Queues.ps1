6a5f65e7466a05c5b2dbd984437770c329dd9283
Surendra <47914424+SurendraMettapalli@users.noreply.github.com>
2024-04-29 23:20:07 +0100

    Add files via upload

--- Chapter01/Create_Azure_Queues.ps1
+++ Chapter01/Create_Azure_Queues.ps1
@@ -19,15 +19,15 @@ $queueName = "<INSERT QUEUE NAME>"
 #        set AZURE_STORAGE_CONNECTION_STRING environment variable); some shells will require
 #        quoting to preserve literal character interpretation.
 
-# NOTE: The following env variable syntax works for Powershell on Windows. You might have to set the env variables differently for other OS.
-$env:AZURE_STORAGE_ACCOUNT=$storageAccount
-$env:AZURE_STORAGE_KEY=$storageKey
+#   NOTE: The following env variable syntax works for Powershell on Windows. You might have to set the env variables differently for other OS.
+    $env:AZURE_STORAGE_ACCOUNT=$storageAccount
+    $env:AZURE_STORAGE_KEY=$storageKey
 
-#You can create a new Azure queue using the storage queue create command:
-az storage queue create --name $queueName --account-name $storageAccount
-#You can easily list the queues under a storage account using the storage queue list term:
-az storage queue list --account-name $storageAccount
-#You can add a new message to the newly created Queue using the storage message put option:
-az storage message put --queue-name $queueName --content "test"
-#Finally, use the storage message peek command to view the message. This command retrieves one or more messages from the front of the queue but does not alter the visibility of the message:
-az storage message peek --queue-name $queueName
+#   You can create a new Azure queue using the storage queue create command:
+    az storage queue create --name $queueName --account-name $storageAccount
+#   You can easily list the queues under a storage account using the storage queue list term:
+    az storage queue list --account-name $storageAccount
+#   You can add a new message to the newly created Queue using the storage message put option:
+    az storage message put --queue-name $queueName --content "test"
+#   Finally, use the storage message peek command to view the message. This command retrieves one or more messages from the front of the queue but does not alter the visibility of the message:
+    az storage message peek --queue-name $queueName
