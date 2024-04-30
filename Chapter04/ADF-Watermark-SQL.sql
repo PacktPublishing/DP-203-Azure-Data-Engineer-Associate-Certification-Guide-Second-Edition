0994f93e9bb2c72193c8513945eb7f643dc91cd9
Surendra <47914424+SurendraMettapalli@users.noreply.github.com>
2024-04-30 10:44:34 +0100

    Add files via upload

--- Chapter04/ADF-Watermark-SQL.sql
+++ Chapter04/ADF-Watermark-SQL.sql
@@ -5,8 +5,8 @@
 CREATE TABLE FactTrips 
 (
 TripIDF INT,
-customerID INT,
-LastModifiedTime DATETIME
+CustomerID INT,
+LastModifiedTime DATETIME2
 );
 
 -- Insert Sample Values To The FactTrips Table.
@@ -16,6 +16,7 @@ INSERT INTO [dbo].[FactTrips] values (101, 201, CURRENT_TIMESTAMP);
 INSERT INTO [dbo].[FactTrips] values (102, 202, CURRENT_TIMESTAMP);
 
 -- Create a Watermark Table
+DROP TABLE WatermarkTable;
 
 CREATE TABLE WatermarkTable
 (
@@ -23,8 +24,18 @@ CREATE TABLE WatermarkTable
   [WatermarkValue] DATETIME,
 );
 
+INSERT INTO [dbo].[WatermarkTable] VALUES ('FactTrips', CURRENT_TIMESTAMP);
+SELECT * FROM WatermarkTable;
+GO
+
+-- You can either update the Watermark table manually as shown or create a stored procedure and execute it everytime there is an update
+UPDATE [dbo].[WatermarkTable] SET [WatermarkValue] = CURRENT_TIMESTAMP WHERE [TableName] = 'FactTrips';
+
 
 -- Create a SP to update WatermarkTable automatically when the new data comes in.
+
+DROP PROCEDURE uspUpdateWatermark
+GO
 
 CREATE PROCEDURE [dbo].uspUpdateWatermark @LastModifiedtime DATETIME, @TableName VARCHAR(100)
 AS
@@ -33,7 +44,13 @@ UPDATE [dbo].[WatermarkTable]
 SET [WatermarkValue] = @LastModifiedtime 
 WHERE [TableName] = @TableName
 END
+GO
+
+-- Executing the stored procedure
+DECLARE @timestamp AS DATETIME = CURRENT_TIMESTAMP;
+EXECUTE uspUpdateWatermark @LastModifiedtime=@timestamp, @TableName='FactTrips';
+
+SELECT * FROM WatermarkTable;
 
 -- Run the Query to check the LastModifiedTime in the source table (FactTrips)
-
 SELECT MAX(LastModifiedTime) AS NewWatermarkValue FROM FactTrips;
