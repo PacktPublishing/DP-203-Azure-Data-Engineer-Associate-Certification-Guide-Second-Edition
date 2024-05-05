20b31a565baf841398fe88afc208414c9f8bbf2b
Surendra <47914424+SurendraMettapalli@users.noreply.github.com>
2024-05-05 15:57:03 +0100

    Add files via upload

--- Chapter02/Create-EventHubs-Namespace.sh
+++ Chapter02/Create-EventHubs-Namespace.sh
@@ -3,3 +3,6 @@ az group create --name StreamingResourceGroup --location UKSouth
 
 # Create an Event Hubs namespace with 10 partitions
 az eventhubs namespace create --name StreamingNamespace --resource-group StreamingResourceGroup --location UKSouth --sku Standard --capacity 10
+
+# Create an Event Hub within the namespace
+az eventhubs eventhub create --name StreamingEventHub --namespace-name StreamingNamespace --resource-group StreamingResourceGroup
